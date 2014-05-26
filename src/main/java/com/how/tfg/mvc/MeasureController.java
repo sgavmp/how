3776f2d189d06e2596a109300f84b4a10f8ac40e
Sergio Garcia Alonso <sga.vmp@gmail.com>
2014-05-26 16:31:40 +0200

    Modificado MeasureController: Controlar que aplicacion se muestra al acceder.

--- src/main/java/com/how/tfg/mvc/MeasureController.java
+++ src/main/java/com/how/tfg/mvc/MeasureController.java
@@ -1,16 +1,33 @@
 package com.how.tfg.mvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.how.tfg.data.services.ConnectionService;
 
 @Controller
 @RequestMapping("/measure")
 public class MeasureController {
 	
-	private String url_first_measure = "/measure/trello";
+	private ConnectionService service;
+	
+	@Autowired
+	public MeasureController(ConnectionService service) {
+		super();
+		this.service = service;
+	}
 		
 	@RequestMapping({"","/"})
-	public String mainMeasureOrFirstMeasure() {
-		return "redirect:"+url_first_measure;
+	public String mainMeasureOrFirstMeasure(RedirectAttributes redirectAttributes) {
+		if (service.haveAnyConnection())
+			return "redirect:"+getUrlFirstApp();
+		redirectAttributes.addFlashAttribute("error", "web.notconnection");
+		return "redirect:/";
+	}
+	
+	private String getUrlFirstApp() {
+		return "/measure/"+service.getFirstApp();
 	}
 }
