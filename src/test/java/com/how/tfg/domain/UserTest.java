4284bb5d0a6d615166a0221dd4e6a33bfc64fa74
Sergio Garcia Alonso <sga.vmp@gmail.com>
2014-05-26 16:43:22 +0200

    Añadidas dependencias para los Test

--- src/test/java/com/how/tfg/domain/UserTest.java
+++ src/test/java/com/how/tfg/domain/UserTest.java
@@ -4,13 +4,11 @@ package com.how.tfg.domain;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.how.tfg.Application;
 import com.how.tfg.data.domain.User;
-import com.how.tfg.data.repository.UserRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
