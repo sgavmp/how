2dcd4d150a288b1f74b84921c8715a944f06bc9a
Sergio Garcia Alonso <sga.vmp@gmail.com>
2014-05-29 19:26:26 +0200

    Añadido test para el controller principal

---
+++ src/test/java/com/how/tfg/SimpleMainControllerTest.java
@@ -0,0 +1,58 @@
+package com.how.tfg;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port=0")
+@DirtiesContext
+public class SimpleMainControllerTest {
+	@Value("${local.server.port}")
+	private int port = 0;
+
+	@Test
+	public void testHome() throws Exception {
+		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + this.port, String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertTrue("Wrong body (title doesn't match):\n" + entity.getBody(), entity
+				.getBody().contains("<title>HOW</title>"));
+	}
+
+	@Test
+	public void testCss() throws Exception {
+		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + this.port
+						+ "/webjars/bootstrap/3.1.1/css/bootstrap.min.css", String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertTrue("Wrong body:\n" + entity.getBody(), entity.getBody().contains("body"));
+		assertEquals("Wrong content type:\n" + entity.getHeaders().getContentType(),
+				MediaType.valueOf("text/css"), entity.getHeaders().getContentType());
+	}
+	
+	@Test
+	public void testJs() throws Exception {
+		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + this.port
+						+ "/webjars/jquery/2.1.1/jquery.min.js", String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertTrue("Wrong body:\n" + entity.getBody(), entity.getBody().contains("body"));
+		assertEquals("Wrong content type:\n" + entity.getHeaders().getContentType(),
+				MediaType.valueOf("application/javascript"), entity.getHeaders().getContentType());
+	}
+}
