caf1bd046fcb84649bdc1086568e9ee722f823f9
Sergio Garcia Alonso <sga.vmp@gmail.com>
2014-06-01 05:04:04 +0200

    Cambio en la frecuencia de actualizacion de las medidas

--- src/main/java/com/how/tfg/modules/RefreshMeasureTask.java
+++ src/main/java/com/how/tfg/modules/RefreshMeasureTask.java
@@ -22,7 +22,7 @@ public class RefreshMeasureTask {
 	
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(cron="0 0 4,16 * * ?")
+    @Scheduled(cron="0 6 * * * ?")
     public void reportCurrentTime() {
     	System.out.println("Start all refresh " + dateFormat.format(new Date()));
         for (ServiceModuleAbstract<?,?> service : allService) {
